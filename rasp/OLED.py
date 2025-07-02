2035f0d96b790329012f1b33ad1786e7d10aa6a8
ga-serra <gabrielvserra25@gmail.com>
2025-07-02 17:55:10 -0300

    Main Code and OLED Code updated

--- rasp/OLED.py
+++ rasp/OLED.py
@@ -4,9 +4,9 @@ from PIL import Image
 import adafruit_ssd1306
 
 class Display:
-    def __init__(self, width=128, height=64):
+    def __init__(self, SCL_pin, SDA_pin, width=128, height=64):
         # Inicializa a interface I2C
-        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.i2c = busio.I2C(SCL_pin, SDA_pin)
         self.width = width
         self.height = height
 
@@ -20,27 +20,23 @@ class Display:
         self.oled.show()
 
     def display_image(self, image_path):
-        # Abre imagem, converte para 1-bit e redimensiona
+        from PIL import Image
         image = Image.open(image_path).convert("L")
         image = image.resize((self.width, self.height))
-        image = image.point(lambda x: 0 if x < 128 else 255, '1')  # binarização
-
-        # Exibe a imagem no OLED
+        image = image.point(lambda x: 0 if x < 128 else 255, '1')  # Binarização
         self.oled.image(image)
         self.oled.show()
 
     def show_message(self, text, font=None, x=0, y=0):
         from PIL import ImageDraw, ImageFont, Image
-
-        # Cria imagem em branco
         image = Image.new("1", (self.width, self.height))
         draw = ImageDraw.Draw(image)
-
-        # Fonte padrão se não passada
         if font is None:
             font = ImageFont.load_default()
-
-        # Escreve o texto
         draw.text((x, y), text, font=font, fill=255)
         self.oled.image(image)
         self.oled.show()
+    
+    def power_off(self):
+        """Desliga o display (manda apagar e não mostrar mais nada)."""
+        self.oled.poweroff()
--- rasp/main.py
+++ rasp/main.py
@@ -4,9 +4,24 @@ from servo import Servo
 import time
 from dht import DHT11, InvalidCheckSum
 from PicoDHT22 import PicoDHT22
+from OLED import Display
+from buzzer import Buzzer
+from photoresistor import photoresistor
+
+flag_automatic_mode = 0
+
+buzzer_port = 5
+buzzer = Buzzer(buzzer_port)
 
 dht_pin = Pin(4, Pin.OUT, Pin.PULL_DOWN)
 dht_sensor = DHT11(dht_pin)
+
+oled_SDA = 7
+oled_SCL = 22
+oled = Display(oled_SCL,oled_SDA)
+
+lum_pin = 66
+lum = photoresistor(lum_pin)
 
 serial = UART(0, baudrate=115200, tx=Pin(16), rx=Pin(17))
 
@@ -49,15 +64,27 @@ def main():
     doors_state = ('closed', 'closed')
 
     while True:
+        if flag_automatic_mode == 1:
+            temp, hum = dht_sensor.measure()
+            lum.read_sensor()
+            if lum <= 0.5:
+                for led, state in leds_state:
+                    leds_state[led] = True
+            else:
+                for led, state in leds_state:
+                    leds_state[led] = False
+            if temp >= 30:
+                fan.value(1)
+            else:
+                fan.value(0)
+
         if serial.any():
             request = serial.read().decode('utf-8')
             command = request.splitlines()[1]
             print(command)
-
             if(command == "/env"):
-                temp = 21
-                hum = 60
-                lum = 0.32
+                temp, hum = dht_sensor.measure()
+                lum.read_sensor()
 
                 serial.write(str(temp))
                 serial.write(",")
@@ -65,6 +92,111 @@ def main():
                 serial.write(",")
                 serial.write(str(lum))
                 serial.write("\r\n")
+            
+            if(command == "/front-door/0"):
+                doors_state[0] = 'close'
+                control_house(leds_state, doors_state)
+            
+            if(command == "/front-door/1"):
+                doors_state[0] = 'open'
+                control_house(leds_state, doors_state)
+            
+            if(command == "/bath-door/0"):
+                doors_state[1] == 'close'
+                control_house(leds_state, doors_state)
+
+            if(command == "/bath-door/1"):
+                doors_state[1] == 'open'
+                control_house(leds_state, doors_state)
+
+            if(command == '/fan/0'):
+                fan.value(0)
+            
+            if(command == '/fan/1'):
+                fan.value(1)
+            
+            if(command == '/bedroom-light/0'):
+                leds_state['house_white'] = False
+                leds_state['house_green'] = False
+                leds_state['house_red'] = False
+                leds_state['house_blue'] = False
+                control_house(leds_state, doors_state)
+            
+            if(command == '/bedroom-light/1'):
+                leds_state['house_white'] = True
+                leds_state['house_green'] = True
+                leds_state['house_red'] = True
+                leds_state['house_blue'] = True
+                control_house(leds_state, doors_state)
+
+            if(command == '/red-light/0'):
+                leds_state['house_red'] = False
+
+            if(command == '/red-light/1'):
+                leds_state['house_green'] = False
+                leds_state['house_red'] = True
+                leds_state['house_blue'] = False 
+
+            if(command == '/green-light/0'):
+                leds_state['house_green'] = False
+
+            if(command == '/green-light/1'):
+                leds_state['house_green'] = True
+                leds_state['house_red'] = False
+                leds_state['house_blue'] = False   
+
+            if(command == '/blue-light/0'):
+                leds_state['house_blue'] = False
+
+            if(command == '/blue-light/1'):
+                leds_state['house_green'] = False
+                leds_state['house_red'] = False
+                leds_state['house_blue'] = True
+
+            if(command == '/bath-light/0'):
+                leds_state['bath'] = False
+                control_house(leds_state, doors_state)
+            
+            if(command == '/bath-light/1'):
+                leds_state['bath'] = True
+                control_house(leds_state, doors_state)
+            
+            if(command == '/tv/0'):
+                oled.power_off()
+            
+            if(command == '/tv/1'):
+                oled.show_message('Welcome')
+                time.sleep(3)
+                oled.display_image("palmeiras.bmp")
+            
+            if(command == '/mode/movie'):
+                for led, state in leds_state:
+                    leds_state[led] = False
+                doors_state = ('close', 'close')
+                control_house(leds_state, doors_state)
+                oled.display_image("palmeiras.bmp")
+
+            if(command == '/mode/wakeup'):
+                leds_state['house_white'] = True
+                control_house(leds_state, doors_state)
+                song = [('C4', 0.3), ('C4', 0.3), ('D4', 0.3), ('C4', 0.3), ('F4', 0.3), ('E4', 0.6),
+                        ('C4', 0.3), ('C4', 0.3), ('D4', 0.3), ('C4', 0.3), ('G4', 0.3), ('F4', 0.6),
+                        ('C4', 0.3), ('C4', 0.3), ('C5', 0.3), ('A4', 0.3), ('F4', 0.3), ('E4', 0.3), ('D4', 0.3),
+                        ('A#4', 0.3), ('A#4', 0.3), ('A4', 0.3), ('F4', 0.3), ('G4', 0.3), ('F4', 0.6)]
+                buzzer.play_song(song)
+
+            if(command == '/mode/empty'):
+                for led, state in leds_state:
+                    leds_state[led] = False
+                oled.power_off()
+                doors_state = ('close', 'close')
+                fan.value(0)
+
+            if(command == '/mode/automatic'):
+                flag_automatic_mode = 1
+            
+            if(command == '/mode/manual'):
+                flag_automatic_mode = 0
 
         control_house(leds_state, doors_state)
 
